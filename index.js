0cb0467ed8702299e26d499788735d5066edb69b
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2023-11-11 12:17:06 +0545

    d

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // console.log("Hello Sudhan");
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -20,6 +20,6 @@ app.get('/youtube' , (req,res)=>{
   res.send("Welcome to youtube")
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
   })
