0da242e25d83ff9a58bee32bb85fbd8ef177c4a2
sudhanpoudel2 <sudhanpoudel2@gmail.com>
2023-11-14 13:52:24 +0545

    done

---
+++ model/ecommerce/category.model.js
@@ -0,0 +1,10 @@
+import moongose from " mongoose";
+
+const categorySchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    }
+},{timestamps:true});
+
+export const Category = mongoose.model("Category",categorySchema)
---
+++ model/ecommerce/order.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+    productId : {
+        type : mongoose.Schema.Type.ObjectId,
+        ref : "Product"
+    },
+    quantity : {
+        type : Number,
+        required : true
+    }
+});
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type : Number,
+        required : true
+    },
+   
+    orderItem : {
+        type : [orderItemSchema]
+    },
+     address : {
+        type : String,
+        required : true
+     },
+     status :{
+        type : String,
+        enum : ["PENDING","CANCELED","DELIVERED"],
+        default : "PENDING"
+
+     }
+},{timestamps:true});
+
+export const Order = mongoose.model("Order",orderSchema);
